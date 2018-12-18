1f5677d831c40ba8b1b6829073f2d7662ba87634
Deivid Valentin <deivid.valentin@gmail.com>
2018-12-17 22:54:02 -0800

    Created Views and Controller

---
+++ WishList/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WishList.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() {
+            return View("Index");
+        }
+
+        public IActionResult Error() {
+            return View("Error");
+        }
+    }
+}
